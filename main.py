b5444de34d8d8fcd6424952bbcb7d17cbaa2cdff
Regihub <42317575+Regihub@users.noreply.github.com>
2021-02-17 22:56:11 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,93 @@
 def on_button_pressed_a():
-    basic.show_number(input.compass_heading())
+    global azimut, y, x
+    basic.clear_screen()
+    stredKompasu()
+    while True:
+        # 326 - 348 °
+        if input.compass_heading() > 328 and input.compass_heading() <= 349:
+            stredKompasu()
+            while True:
+                led.toggle(1, 0)
+                basic.pause(200)
+                if not (input.compass_heading() > 328 and input.compass_heading() <= 349):
+                    led.unplot(1, 0)
+                    break
+        # 348 - 12 °
+        if input.compass_heading() > 349 or input.compass_heading() <= 11:
+            stredKompasu()
+            while True:
+                led.toggle(2, 0)
+                basic.pause(200)
+                if not (input.compass_heading() > 349 or input.compass_heading() <= 11):
+                    led.unplot(2, 0)
+                    break
+        # 12 - 36 °
+        if input.compass_heading() > 11 and input.compass_heading() <= 34:
+            stredKompasu()
+            while True:
+                led.toggle(3, 0)
+                basic.pause(200)
+                if not (input.compass_heading() > 11 and input.compass_heading() <= 34):
+                    led.unplot(3, 0)
+                    break
+        # 36 - 60 °
+        if input.compass_heading() > 34 and input.compass_heading() <= 56:
+            stredKompasu()
+            while True:
+                led.toggle(4, 0)
+                basic.pause(200)
+                if not (input.compass_heading() > 34 and input.compass_heading() <= 56):
+                    led.unplot(4, 0)
+                    break
+        azimut = 56
+        y = 1
+        # 60 - 156 °
+        while y <= 4:
+            if input.compass_heading() > azimut and input.compass_heading() <= azimut + 23:
+                stredKompasu()
+                while True:
+                    led.toggle(4, y)
+                    basic.pause(200)
+                    if not (input.compass_heading() > azimut and input.compass_heading() <= azimut + 23):
+                        led.unplot(4, y)
+                        break
+            y += 1
+            azimut += 23
+        azimut = 146
+        x = 3
+        # 156 - 252 °
+        while x >= 0:
+            if input.compass_heading() > azimut and input.compass_heading() <= azimut + 22:
+                stredKompasu()
+                while True:
+                    led.toggle(x, 4)
+                    basic.pause(200)
+                    if not (input.compass_heading() > azimut and input.compass_heading() <= azimut + 22):
+                        led.unplot(x, 4)
+                        break
+            x += -1
+            azimut += 22
+        azimut = 236
+        y = 3
+        # 252 - 348 °
+        while y >= 0:
+            if input.compass_heading() > azimut and input.compass_heading() <= azimut + 23:
+                stredKompasu()
+                while True:
+                    led.toggle(0, y)
+                    basic.pause(200)
+                    if not (input.compass_heading() > azimut and input.compass_heading() <= azimut + 23):
+                        led.unplot(0, y)
+                        break
+            y += -1
+            azimut += 23
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def stredKompasu():
+    led.plot_brightness(2, 1, 150)
+    led.plot_brightness(1, 2, 150)
+    led.plot_brightness(3, 2, 150)
+    led.plot_brightness(2, 3, 150)
 
 def on_gesture_shake():
     soundExpression.hello.play()
@@ -11,9 +98,7 @@ def on_logo_touched():
     basic.clear_screen()
 input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_touched)
 
+x = 0
+y = 0
+azimut = 0
 input.calibrate_compass()
-for y in range(5):
-    for x in range(5):
-        led.plot(x, y)
-        basic.pause(70)
-basic.clear_screen()
--- main.ts
+++ main.ts
@@ -1,60 +1,117 @@
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    stredKompasu()
     while (true) {
-        basic.clearScreen()
-        images.createImage(`
-            . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
-            `).showImage(0)
-        if (input.compassHeading() < 11) {
+        // 326 - 348 °
+        if (input.compassHeading() > 328 && input.compassHeading() <= 349) {
+            stredKompasu()
+            while (true) {
+                led.toggle(1, 0)
+                basic.pause(200)
+                if (!(input.compassHeading() > 328 && input.compassHeading() <= 349)) {
+                    led.unplot(1, 0)
+                    break;
+                }
+            }
+        }
+        // 348 - 12 °
+        if (input.compassHeading() > 349 || input.compassHeading() <= 11) {
+            stredKompasu()
             while (true) {
                 led.toggle(2, 0)
                 basic.pause(200)
-                if (input.compassHeading() > 11) {
+                if (!(input.compassHeading() > 349 || input.compassHeading() <= 11)) {
+                    led.unplot(2, 0)
                     break;
                 }
             }
         }
-        if (input.compassHeading() >= 11 && input.compassHeading() < 34) {
+        // 12 - 36 °
+        if (input.compassHeading() > 11 && input.compassHeading() <= 34) {
+            stredKompasu()
             while (true) {
                 led.toggle(3, 0)
                 basic.pause(200)
-                if (!(input.compassHeading() <= 11 && input.compassHeading() > 34)) {
+                if (!(input.compassHeading() > 11 && input.compassHeading() <= 34)) {
+                    led.unplot(3, 0)
                     break;
                 }
             }
         }
-        if (input.compassHeading() >= 34 && input.compassHeading() < 56) {
+        // 36 - 60 °
+        if (input.compassHeading() > 34 && input.compassHeading() <= 56) {
+            stredKompasu()
             while (true) {
                 led.toggle(4, 0)
                 basic.pause(200)
-                if (!(input.compassHeading() <= 34 && input.compassHeading() > 56)) {
+                if (!(input.compassHeading() > 34 && input.compassHeading() <= 56)) {
+                    led.unplot(4, 0)
                     break;
                 }
             }
         }
-        if (input.compassHeading() >= 56 && input.compassHeading() < 78) {
-            while (true) {
-                led.toggle(4, 1)
-                basic.pause(200)
-                if (!(input.compassHeading() <= 56 && input.compassHeading() > 78)) {
-                    break;
+        azimut = 56
+        y = 1
+        // 60 - 156 °
+        while (y <= 4) {
+            if (input.compassHeading() > azimut && input.compassHeading() <= azimut + 23) {
+                stredKompasu()
+                while (true) {
+                    led.toggle(4, y)
+                    basic.pause(200)
+                    if (!(input.compassHeading() > azimut && input.compassHeading() <= azimut + 23)) {
+                        led.unplot(4, y)
+                        break;
+                    }
                 }
             }
+            y += 1
+            azimut += 23
         }
-        if (input.compassHeading() >= 78 && input.compassHeading() < 101) {
-            while (true) {
-                led.toggle(4, 2)
-                basic.pause(200)
-                if (!(input.compassHeading() <= 78 && input.compassHeading() > 101)) {
-                    break;
+        azimut = 146
+        x = 3
+        // 156 - 252 °
+        while (x >= 0) {
+            if (input.compassHeading() > azimut && input.compassHeading() <= azimut + 22) {
+                stredKompasu()
+                while (true) {
+                    led.toggle(x, 4)
+                    basic.pause(200)
+                    if (!(input.compassHeading() > azimut && input.compassHeading() <= azimut + 22)) {
+                        led.unplot(x, 4)
+                        break;
+                    }
                 }
             }
+            x += -1
+            azimut += 22
+        }
+        azimut = 236
+        y = 3
+        // 252 - 348 °
+        while (y >= 0) {
+            if (input.compassHeading() > azimut && input.compassHeading() <= azimut + 23) {
+                stredKompasu()
+                while (true) {
+                    led.toggle(0, y)
+                    basic.pause(200)
+                    if (!(input.compassHeading() > azimut && input.compassHeading() <= azimut + 23)) {
+                        led.unplot(0, y)
+                        break;
+                    }
+                }
+            }
+            y += -1
+            azimut += 23
         }
     }
 })
+function stredKompasu () {
+    led.plotBrightness(2, 1, 150)
+    led.plotBrightness(1, 2, 150)
+    led.plotBrightness(3, 2, 150)
+    led.plotBrightness(2, 3, 150)
+}
 input.onGesture(Gesture.Shake, function () {
     soundExpression.hello.play()
     basic.showNumber(randint(1, 6))
@@ -62,4 +119,7 @@ input.onGesture(Gesture.Shake, function () {
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     basic.clearScreen()
 })
+let x = 0
+let y = 0
+let azimut = 0
 input.calibrateCompass()
